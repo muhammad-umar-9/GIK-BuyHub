69f817139a2a5fdbe8640591232e18b5ff24bcb2
Muhammad Umar <167021629+muhammad-umar-9@users.noreply.github.com>
2025-05-14 23:50:31 +0500

    Update postgres.js

--- backend/dbdrivers/postgres.js
+++ backend/dbdrivers/postgres.js
@@ -1,1 +1,15 @@
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres', // Your PostgreSQL database name
+    password: 'Admin',         // Your PostgreSQL password
+    port: 5432,             // Your PostgreSQL port
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+    type: 'postgres',
+};
