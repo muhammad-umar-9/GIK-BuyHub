e0b0850c6ab4f2aab375002ac87ed37d55ffeaeb
Muhammad Umar <167021629+muhammad-umar-9@users.noreply.github.com>
2025-05-14 23:51:27 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,11 @@
 
+const DB_TYPE = process.env.DB_TYPE || 'postgres'; // Default to Postgres
+let driver;
+
+if (DB_TYPE === 'mongodb') {
+    driver = require('./dbdrivers/mongodb');
+} else {
+    driver = require('./dbdrivers/postgres');
+}
+
+module.exports = driver;
