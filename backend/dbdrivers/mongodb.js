cd82601b84a5007a43bf4b033806b3cca68b5748
Muhammad Umar <167021629+muhammad-umar-9@users.noreply.github.com>
2025-05-14 23:49:30 +0500

    Update mongodb.js

--- backend/dbdrivers/mongodb.js
+++ backend/dbdrivers/mongodb.js
@@ -1,1 +1,22 @@
 
+const { MongoClient, ObjectId } = require('mongodb');
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017'; // Or your MongoDB connection string
+const MONGO_DB = process.env.MONGO_DB || 'gikihub'; // Your MongoDB database name
+
+const client = new MongoClient(MONGO_URI, { useUnifiedTopology: true });
+let db;
+
+async function connect() {
+    if (!db) {
+        await client.connect();
+        db = client.db(MONGO_DB);
+    }
+    return db;
+}
+
+module.exports = {
+    connect,
+    ObjectId, // Export ObjectId directly
+    type: 'mongodb',
+};
